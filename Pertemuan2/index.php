81921d4345116d7c7ece728b436950de509147b6
unknown <anganfebriana90@gmail.com>
2022-11-03 10:34:14 +0800

    kuliah hari ini 03/11/22

---
+++ Pertemuan2/index.php
@@ -0,0 +1,33 @@
+<?php 
+// standar output di php
+//  echo, print
+
+//var_dump ("Hello world!");
+
+//$nama = "Angan Febriana";
+//echo "Hello, Nama saya $nama";
+
+// operator
+// aritmatika
+// * / + %
+
+// $x =  10;
+// $y = 20;
+// echo $x + $y;
+
+// // penggabungan string / concatenation / concat
+// // .
+// $nama_depan = "Angan";
+// $nama_belakang = "Febriana";
+// echo $nama_depan ." ". $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=
+
+// //$x = 1;
+// $x -= 5;
+// echo $x;
+$nama .= $nama_depan;
+$nama .= " ";
+$nama .= $nama_belakang;
+echo $nama;
