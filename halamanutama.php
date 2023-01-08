c5522c62c75c2de5a742a556fc6418c886c26a5e
anganfebriana <anganfebriana90@gmail.com>
2023-01-08 18:24:45 +0800

    UAS angan febriana 20241011

---
+++ halamanutama.php
@@ -0,0 +1,124 @@
+<!doctype html>
+    <html class="no-js" lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Dashboard V.2 | Kiaalap - Kiaalap Admin Template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    </head>
+
+    <body>
+
+        <style type="text/css">
+
+            .navbar-siad{
+                background: #006df0;
+                border-radius: 0px;
+                border: 1px solid #006df0;
+                margin: 0px;
+                padding: 20px 0px;
+            }
+
+            .navbar-inverse .navbar-brand {
+
+                color: #fff;
+
+            }
+
+            .navbar-inverse .navbar-nav > li > a {
+
+                color: #fff;
+
+            }
+            .navbar-siad > li > a {
+
+                color: #9d9d9d !important;
+
+            }
+
+            .banner{
+                background: #006df0;
+                border-radius: 0px;
+                border: 1px solid #006df0;
+                padding: 60px 0px;
+            }
+
+            .banner{
+                color: white;
+            }
+
+            .banner a{
+                padding: 15px 25px;
+                color: white;
+                border: 1px solid white;
+                -webkit-transition: all 0.5s; 
+                transition: all 0.5s;
+                margin-right: 10px;
+            }
+
+            .banner a:hover{
+                text-decoration: none;
+                border: 1px dashed white;
+            }
+
+            .banner p{
+                font-size: 13pt;
+            }
+        </style>
+
+
+
+        <nav class="navbar navbar-inverse navbar-siad">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                    <a class="navbar-brand" href="#">Sistem Informasi</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    
+
+                   
+                </div>
+            </div>
+        </nav>
+
+
+        <div class="banner">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div style="margin-top: 140px;">
+                            <h1>Sistem Informasi Mahasiswa</h1>
+                            <p>Prodi Pendidikan Teknologi Informasi</p>
+
+                            <br>
+                            <br>
+
+                            <a href="login.php">LOGIN ADMIN</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+
+                        <img src="background.png">
+                        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+
+        <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+
+    </html>
